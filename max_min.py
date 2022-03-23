b98572853d521ba7321539243f0b7094782a2726
Erbol <777baktybekov777@gmail.com>
2022-03-23 18:36:51 +0200

    homew_work

---
+++ max_min.py
@@ -0,0 +1,3 @@
+a , b , c  = int(input('введите число: ')) , int(input('введите число: ')) , int(input('введите число: '))
+print(max(a , b , c))
+print(min(a , b , c))
---
+++ proverka_chisla.py
@@ -0,0 +1,8 @@
+#
+a = int(input('Введите число: '))
+if a < 10 :
+    print('число меньше 10')
+elif a >= 10 and a < 100:
+    print(a)
+else:
+    print('число больше 100')
